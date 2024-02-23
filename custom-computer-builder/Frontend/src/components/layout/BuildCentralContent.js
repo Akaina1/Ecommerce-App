1816ff262daeb1108a1b8b75e5585b846110622a
Akaina1 <erinadost@gmail.com>
2024-02-23 16:55:35 -0500

    Custom Build rework

--- custom-computer-builder/Frontend/src/components/layout/BuildCentralContent.js
+++ custom-computer-builder/Frontend/src/components/layout/BuildCentralContent.js
@@ -1,45 +1,10 @@
 import React from 'react';
 import { Container } from '@mui/material';
-import svgArray from '../common/CustomBuildSVG';
-import PartSlot from '../common/PartSlot'; 
 
 function BuildCentralContent({ aestheticState }) {
   return (
     <Container maxWidth={false} className="build-central-content">
-      {svgArray.map((svg) => {
-        let opacity = aestheticState[svg.id] ? 1 : 0;
-        // Check if the part is customizable
-        const isCustomizable = [
-          'case',
-          'CPU',
-          'GPU',
-          'MOBO',
-          'PSU',
-          'RAM',
-          'SSD',
-          'HDD',
-          'frontFans',
-          'backFans',
-          'cooling'
-        ].includes(svg.id);
-
-        if (isCustomizable) {
-          // Render a PartSlot component for customizable parts
-          return <PartSlot 
-            key={svg.id} 
-            className={svg.id} 
-            partType={svg.id} 
-            partSVG={svg.src} />;
-        } else {
-          // Otherwise render it as a regular image
-          return <img 
-            key={svg.id} id={svg.id} 
-            className={svg.id} 
-            src={svg.src} 
-            alt={svg.alt}  
-            style={{ opacity }} />;
-        }
-      })}
+     {/* // rework incoming */}
     </Container>
   );
 }
